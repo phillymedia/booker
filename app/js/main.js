f2ec6ddd7aeb91a3623d4e6111f07a3c5bc374dc
jared-whalen <jwhalen@philly.com>
2018-04-18 17:38:41 -0400

    Init

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,26 @@
-$(document).ready(function(){
+require('./smooth-scroll.js')
+
+$(document).ready(function() {
+
+  $(window).on("load resize", function(e) {
+    $('.max-width-video-wrapper').each(function() {
+      this.style.setProperty('max-width', '100%', 'important');
+    });
+
+    var th = $("#text-container").height(),
+      header = $(".header").height(),
+      w = $(window).height(),
+      com = th + header;
+
+    $("#booker-top, .mobile-top").css("margin-top", header);
+  });
+
+  // $("#trigger.videoWrapper").find("iframe").css("height",(w-header))
+
+  $(".quote").each(function() {
+    $(this).prepend("<span class='quote-large left-q'>&ldquo;</span>")
+    // .append("<span class='quote-large right-q'>&rdquo;</span>")
+    ;
+  })
+
 });
